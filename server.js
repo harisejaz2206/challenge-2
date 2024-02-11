02aab2ccfd81185e71bcfc31e0389513e75039c6
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-12 04:30:02 +0500

    thought process of making a server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 /*
 I am assuming that you want me to complete this challenge using nodejs and not use any of Node.js's frameworks. I hope I am right.
 */
+
+// Structure
+// 1. Define a local port for the server
+// 2. create an http server
+// 2. b) handlle routing in createServer callback
+// 3. start the server
