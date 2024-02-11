36cc2686b6e8f952f3649322478bb1ac2ab02c1a
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-12 04:45:31 +0500

    http server setup complete and tested on local browser

--- server.js
+++ server.js
@@ -7,3 +7,30 @@ I am assuming that you want me to complete this challenge using nodejs and not u
 // 2. create an http server
 // 2. b) handlle routing in createServer callback
 // 3. start the server
+
+const http = require("http");
+
+const PORT = 8181;
+
+const server = http.createServer((req, res) => {
+  // log each request method and url
+  console.log(`${req.method} ${req.url}`);
+
+  if (req.url === "/") {
+    // handle root route logic
+    res.writeHead(200), { "Content-Type": "text/plain" };
+    res.end("This is the homepage!");
+  } else if (req.url === "/about") {
+    // handle /about route logic
+    res.writeHead(200), { "Content-Type": "text/plain" };
+    res.end("This is the about page!");
+  } else {
+    // handle non-existent route logic for error handling
+    res.writeHead(404);
+    res.end("404 not found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
